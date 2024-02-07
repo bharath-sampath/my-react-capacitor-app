a6e128e1c61fb780171f4067db7b63d9f4963a60
Administrator <admin@example.com>
2024-02-08 05:12:06 +0530

    capacitor config updated

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,9 +4,12 @@ const config: CapacitorConfig = {
   appId: 'com.example.myreactcapacitorapp',
   appName: 'my-react-capacitor-app',
   webDir: 'build',
+  bundledWebRuntime: false,
   server: {
-    androidScheme: 'https'
+    url: "https://my-react-capacitor-k8c5n61yn-bharath-sampath.vercel.app/",
+    allowNavigation: ["my-react-capacitor-k8c5n61yn-bharath-sampath.vercel.app"]
   }
 };
 
 export default config;
+
